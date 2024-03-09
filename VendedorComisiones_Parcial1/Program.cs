bc96d1573bc9ab864c3f43b6660c077e3f91863f
MAURICIO ALEXANDER CARDONA SANCHEZ <mauriciocardona97996@correo.itm.edu.co>
2024-03-08 20:48:02 -0500

    segundo commit

--- VendedorComisiones_Parcial1/Program.cs
+++ VendedorComisiones_Parcial1/Program.cs
@@ -10,10 +10,18 @@ namespace VendedorComisiones_Parcial1
     {
         static void Main(string[] args)
         {
+            
             // declaracion de variables
 
             double sueldoBase,ventas1,ventas2,ventas3,comision,totalcomisiones,sueldoFinal;
+            double mayorComision,promedioComisioines
 
+
+            // entrada de datos
+            Console.WriteLine(" Inngrese sueldo base del vendedor$");
+            sueldoBase = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < ventas.Legth; i++);
 
         }
     }
