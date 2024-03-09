ef1576a493048e39921800bf881215de833b5a90
MAURICIO ALEXANDER CARDONA SANCHEZ <mauriciocardona97996@correo.itm.edu.co>
2024-03-08 20:33:15 -0500

    parcial 1 comision vendedor

---
+++ VendedorComisiones_Parcial1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendedorComisiones_Parcial1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaracion de variables
+
+            double sueldoBase,ventas1,ventas2,ventas3,comision,totalcomisiones,sueldoFinal;
+
+
+        }
+    }
+}
