a981cca0058a9115ae668dcc13aefc914fb7b020
MAURICIO ALEXANDER CARDONA SANCHEZ <mauriciocardona97996@correo.itm.edu.co>
2024-03-08 21:49:38 -0500

    agregando los beneficios

--- VendedorComisiones_Parcial1/Program.cs
+++ VendedorComisiones_Parcial1/Program.cs
@@ -13,7 +13,7 @@ namespace VendedorComisiones_Parcial1
             
             // declaracion de variables
 
-            double sueldoBase,ventas1,ventas2,ventas3,comision,totalcomisiones,sueldoFinal;
+            double sueldoBase,venta1,venta2,venta3,comision,totalcomisiones,sueldoFinal;
             double mayorComision, promedioComisioines;
             double[]ventas = new double[3];
             const double PORCENTAJE_COMISION = 0.10;
@@ -27,10 +27,34 @@ namespace VendedorComisiones_Parcial1
 
             for (int i = 0; i < ventas.Length; i++);
             {
-                Console.WriteLine($"Ingrese el valor de la venta{ i+1}:$");
-                    
+                Console.WriteLine($"Ingrese el valor de la venta{i+1}:$");
+                ventas[i]=double.Parse(Console.ReadLine());
             }
 
+            // Proceso
+            venta1 = ventas[0];
+            venta2 = ventas[1];
+            venta3 = ventas[2];
+            comision = (venta1 + venta2 + venta3) * PORCENTAJE_COMISION;
+            totalcomisiones = comision;
+            sueldoFinal = sueldoBase + totalcomisiones;
+            mayorComision = Math.Max(Math.Max(venta1 * PORCENTAJE_COMISION, venta2 * PORCENTAJE_COMISION), venta3 * PORCENTAJE_COMISION);
+            promedioComisioines = totalcomisiones / 3;
+
+            // salida de resultados
+
+            Console.WriteLine($"\nDinero obtenido por concepto de comisiones: $ {totalcomisiones}");
+            Console.WriteLine($"\nTotal a recibir en el mes: $ {sueldoFinal}");
+            Console.WriteLine($"\nLa venta que genero la mayor comision fue: $ {mayorComision}");
+            Console.WriteLine($"\nEl promedio de las comisiones por venta fue: $ {promedioComisioines}");
+
+            // Verificacion del beneficio extra
+            if(venta1+venta2+venta3 >= OBJETIVO_VENTAS)
+            {
+                sueldoFinal += BENEFICIO_EXTRA;
+                Console.WriteLine("Felicidades has ganado el beneficio extra de $ 100.000.");
+                Console.WriteLine($"El sueldo final con el beneficio es:$"{sueldoFinal});
+            }
         }
     }
 }
